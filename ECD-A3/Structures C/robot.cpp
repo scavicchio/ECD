f464e99756b97bdbe9875362110d671dd9fe83ea
Salvy Cavicchio <scavicchio@users.noreply.github.com>
2019-11-26 14:32:32 -0500

    robot simulator implement

--- ECD-A3/Structures C/robot.cpp
+++ ECD-A3/Structures C/robot.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "robot.hpp"
+
+void robot::simulate(bool multicore = false, int maxSteps = 1, bool pulse = false) {
+    vector<force> forces;
+    
+    while (maxSteps > 0) {
+         forces.clear();
+         // get the forces
+        // this can become parallel later
+        for (vector<mass>::iterator item = masses.begin(); item != masses.end(); item++) {
+            forces.push_back(force(&(*item),pulse));
+        }
+        int i = 0;
+        // this can also become parallel later
+        for (vector<mass>::iterator item = masses.begin(); item != masses.end(); item++) {
+            item->updateDerivitives(forces[i]);
+            i++;
+        }
+        t = t+=timestep;
+        maxSteps--;
+    }
+    return;
+}
