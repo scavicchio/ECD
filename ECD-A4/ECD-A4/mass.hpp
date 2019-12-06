faafb5a1470e939a378862fbb540aafc0a49b26e
scavicchio <salvy@nyu.edu>
2019-12-06 16:10:49 -0500

    forces except spring

--- ECD-A4/ECD-A4/mass.hpp
+++ ECD-A4/ECD-A4/mass.hpp
@@ -54,6 +54,46 @@ struct mass {
         f[0] = 0; f[1] = 0; f[2] = 0;
     };
     
+
+    void addGravityForce() {
+        for (int i = 0; i < 3; i++) {
+            f[i] += weight*g[i];
+        }
+        return;
+    }
+
+    void addFrictionForce() {
+        // will only add friction force if the body is on (or below) the ground AND the normal force is < 0
+        if (p[1] <= 0 && f[1] < 0) {
+            // a friction component is calculated as f_i = fN*u
+            double fN = f[1]*friction_mu_k;
+        //    double fH = sqrt(pow(f[0],2)+pow(f[2],2));
+            if (f[0] < fN) { // then zero the velocity (ie the friction is slowing the mass)
+                v[0] = 0;
+            }
+            else if (f[0] > 0) { f[0] -= fN; }
+            else { f[0] += fN; }
+            
+            if (f[2] < fN) { // then zero the velocity (ie the friction is slowing the mass)
+                v[2] = 0;
+            }
+            else if (f[2] > 0) { f[2] -= fN; }
+            else { f[2] += fN; }
+        }
+        return;
+    }
+
+    void addResultantForce() {
+        if (p[1] < 0) {
+            // for bouncing on the ground
+      //      body->p[1] = 0;
+        //    body->v[1] = 0;
+          //  body->a[1] = 0;
+            f[1] -= kc*(p[1]);
+        }
+        return;
+    }
+
     void updateDerivitives(const double d = damping) {
         if (!fixed) {
             for (int i = 0; i < 3; i++) {
